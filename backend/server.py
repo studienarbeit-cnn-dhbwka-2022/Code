9662f96191495c678dd38a27b42cd6cac8bd62cb
magoekce <magoekce@united-internet.de>
2023-03-10 16:34:04 +0100

    feat:  Uploading images now works

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
+import logging
+from pathlib import Path
+from time import sleep
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI()
 
@@ -18,8 +20,14 @@ app.add_middleware(
 
 @app.get("/img")
 async def read_image(path: str):
-    image_path = f"./img/{path}"
-    return FileResponse(image_path)
+    image_path = Path(f"./img/{path}")
+    logging.info(f"image file: {image_path}")
+    if image_path.is_absolute() or str(image_path).startswith(".."):
+        raise HTTPException(status_code=403, detail="Forbidden")
+    if not image_path.exists() or not image_path.is_file():
+        raise HTTPException(status_code=404, detail="File not found")
+    logging.info(f"Serving image file: {image_path}")
+    return FileResponse(str(image_path))
 
 
 @app.post("/img")
@@ -36,4 +44,6 @@ async def create_upload_file(file: UploadFile = File(...)):
     finally:
         file.file.close()
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    sleep(1)
+
+    return {"src": f"http://127.0.0.1:8000/img?path={file.filename}"}
--- frontend/main.js
+++ frontend/main.js
@@ -8,35 +8,54 @@ const loadFile = function (event) {
     }
 }
 
-const element = document.querySelector('form');
-element.addEventListener('submit', event => {
-    event.preventDefault();
-    // actual logic, e.g. validate the form
-    console.log('Form submission cancelled.');
+const form = document.querySelector('form');
+form.addEventListener('submit', uploadImage);
 
-    const input = document.getElementById("imageInput");
+function uploadImage(event) {
+    event.preventDefault();
+
+    const input = document.querySelector('#imageInput');
     const file = input.files[0];
-    if (!file) {
-        alert("Please select an image file.");
-        return;
-    }
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                // The response text is the URL of the processed image
-                const imageUrl = xhr.responseText;
-                // Update the placeholder image with the processed image
-                const placeholderImage = document.getElementById("out");
-                placeholderImage.src = imageUrl;
-            } else {
-                alert("An error occurred while processing the image.");
-            }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post('http://127.0.0.1:8000/img', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
         }
-    };
-    xhr.open("POST", "http://127.0.0.1:8000/img");
-    xhr.setRequestHeader("Content-Type", "application/octet-stream");
-    xhr.send(file);
-})
+    })
+        .then(response => {
+            console.log(response.data.src)
+
+            const results = document.getElementById('results');
+
+            const newChild = document.createElement('div');
+            newChild.classList.add('col-lg-3', 'col-sm-6');
+
+            const newTitle = document.createElement('h2');
+            newTitle.classList.add('pt-3');
+            newTitle.textContent = response.data.src.title;
+
+            const newImage = document.createElement('img');
+            newImage.src = response.data.src;
+            newImage.classList.add('img-fluid', 'rounded-1');
+            newImage.alt = response.data.src.alt;
+
+            newChild.appendChild(newTitle);
+            newChild.appendChild(newImage);
+
+            results.appendChild(newChild);
+
+            // The response text is the URL of the processed image
+            const imageUrl = response.data.src;
+            // Update the placeholder image with the processed image
+            const placeholderImage = document.getElementById("out");
+            placeholderImage.src = imageUrl;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
 
