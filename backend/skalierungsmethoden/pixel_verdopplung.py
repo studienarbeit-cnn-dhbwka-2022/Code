c2fcd0ceead6f17e66a972e201a49852481875f7
Lukas Hörnle <Lukas.hoernle@web.de>
2023-03-12 12:37:44 +0100

    Update pixel_verdopplung.py

--- backend/skalierungsmethoden/pixel_verdopplung.py
+++ backend/skalierungsmethoden/pixel_verdopplung.py
@@ -1,3 +1,20 @@
 def pixel_verdopplung(image):
+import numpy as np
+import cv2
 
+def pixel_doubling(image, scale_factor):
+    new_size = (int(image.shape[1] * scale_factor), int(image.shape[0] * scale_factor))
+    
+    scaled_image = np.zeros(new_size + (image.shape[2],), dtype=np.uint8)
+    for i in range(new_size[0]):
+        for j in range(new_size[1]):
+            x = int(i / scale_factor)
+            y = int(j / scale_factor)
+            scaled_image[j, i] = image[y, x]
+    
+    return scaled_image
+
+image = cv2.imread('cactus.jpg')
+scaled_image = pixel_doubling(image, 2)
+cv2.imshow(scaled_image)
     pass
