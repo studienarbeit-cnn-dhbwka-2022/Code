f97ad2c52297adfc67914aa750f7b8f5e8137b98
Marc <71791731+MKrabs@users.noreply.github.com>
2023-03-17 13:01:51 +0000

    fix(back): normalization bilinear interpolation

--- backend/skalierungsmethoden/bilinear_interpolation.py
+++ backend/skalierungsmethoden/bilinear_interpolation.py
@@ -31,6 +31,13 @@ class BilinearInterpolation(Image):
                 w3 = (x2 - x_old) * (y_old - y1)
                 w4 = (x_old - x1) * (y_old - y1)
 
+                # Normalize the weights
+                w_sum = w1 + w2 + w3 + w4
+                w1 /= w_sum
+                w2 /= w_sum
+                w3 /= w_sum
+                w4 /= w_sum
+
                 # Get the pixel values of the surrounding pixels
                 p1 = self.img.getpixel((x1, y1))
                 p2 = self.img.getpixel((x2, y1))
